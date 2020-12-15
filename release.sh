60e6a1554fcc03554838a5d2cde688df4ceda2dc
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2020-12-15 22:54:40 +0100

    Add scripts

---
+++ release.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+rm -rf release
+mkdir -p release
+cp -rf Analysis *.{hpp,cpp,json} release/
+mkdir -p release/3rdparty/Gist
+cp -rf 3rdparty/Gist/src \
+       3rdparty/Gist/LICENSE.txt \
+       release/3rdparty/Gist/
+
+mv release score-addon-analysis
+7z a score-addon-analysis.zip score-addon-analysis
+
+
+
