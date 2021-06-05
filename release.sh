ef937a4d74486b198cead75794f59914d9b3caeb
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2021-06-05 10:59:50 +0200

    release

--- release.sh
+++ release.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 rm -rf release
 mkdir -p release
-cp -rf Analysis *.{hpp,cpp,json} CMakeLists.txt release/
+
+cp -rf Analysis *.{hpp,cpp,txt} release/
+
+VERSION=$(git tag)
+cat addon.json | sed  "s/\$VERSION/$VERSION/g" > release/addon.json
+
 mkdir -p release/3rdparty/Gist
 cp -rf 3rdparty/Gist/src \
        3rdparty/Gist/LICENSE.txt \
@@ -9,6 +14,3 @@ cp -rf 3rdparty/Gist/src \
 
 mv release score-addon-analysis
 7z a score-addon-analysis.zip score-addon-analysis
-
-
-
